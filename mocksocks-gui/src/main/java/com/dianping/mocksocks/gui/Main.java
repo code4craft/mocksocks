77bfd91e6424f8117235f2e44ba1afe0b09073de
yihua.huang <code4crafter@gmail.com>
2013-10-24 22:37:17 +0800

    invite jgoodies

--- mocksocks-gui/src/main/java/com/dianping/mocksocks/gui/Main.java
+++ mocksocks-gui/src/main/java/com/dianping/mocksocks/gui/Main.java
@@ -1,6 +1,8 @@
 package com.dianping.mocksocks.gui;
 
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * @author yihua.huang@dianping.com
@@ -9,6 +11,16 @@ public class Main {
     private JList list1;
     private JPanel panel;
     private JFormattedTextField sdasdsadFormattedTextField;
+
+    public Main() {
+        list1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                System.out.println(e.getKeyChar());
+                super.keyTyped(e);    //To change body of overridden methods use File | Settings | File Templates.
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Main");
