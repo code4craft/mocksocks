913cda6cc9daa1f0a966434af5c4373a491a54fe
yihua.huang <code4crafter@gmail.com>
2013-10-30 09:30:35 +0800

    message

--- mocksocks-gui/src/main/java/com/dianping/mocksocks/gui/gui/MessageDetailDialog.java
+++ mocksocks-gui/src/main/java/com/dianping/mocksocks/gui/gui/MessageDetailDialog.java
@@ -5,6 +5,7 @@ import com.dianping.mocksocks.proxy.message.Exchange;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class MessageDetailDialog extends JDialog {
     private JPanel contentPane;
@@ -30,12 +31,22 @@ public class MessageDetailDialog extends JDialog {
                 onOK();
             }
         });
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
     public void setExchange(Exchange exchange) {
         this.exchange = exchange;
         editorRequest.setText(exchange.getRequest().outputBuffer());
         editorResponse.setText(exchange.getResponse().outputBuffer());
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
     }
 
     private void onOK() {
